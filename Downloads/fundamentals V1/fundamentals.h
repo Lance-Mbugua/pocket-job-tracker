afbedc747e79dcfbfd2c64749c7bd00845c52578
Lance <limbugua@myseneca.ca>
2024-12-04 20:02:08 -0500

    Version_1

---
+++ Downloads/fundamentals V1/fundamentals.h
@@ -0,0 +1,18 @@
+// FUNDAMENTALS MODULE HEADER
+// Author: Lance Mbugua, limbugua@myseneca.ca, 144111226, 2024/11/28
+// Course: CPR101NII
+// Project: fundamentals : version 1
+// Purpose: This header file declares the fundamentals module, which includes
+//          necessary libraries and the prototype for the fundamentals function.
+
+#ifndef _FUNDAMENTALS_H_
+#define _FUNDAMENTALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function prototype for the fundamentals function
+void fundamentals(void);
+
+#endif
